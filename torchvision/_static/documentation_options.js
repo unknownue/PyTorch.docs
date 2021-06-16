c39473b4ae6a1443804b1d5bf010a31bf6faead9
unknownue <unknownue@outlook.com>
2021-06-16 10:34:55 +0800

    :bookmark: Publish docs for PyTorch v1.9.0 and torchvision v0.10.0.

--- torchvision/_static/documentation_options.js
+++ torchvision/_static/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: 'master',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.10.0+cu102',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true
 };
